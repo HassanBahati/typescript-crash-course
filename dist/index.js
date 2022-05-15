21c5a1e3fb89f1f7f36b35df64be6726a85b5661
HassanBahati <mukisabahati@gmail.com>
2022-05-15 12:40:01 +0300

    add data types

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,42 @@
 "use strict";
+// Basic types
 let id = 5;
+let company = "Hassan Bahati";
+let isPublished = true;
+let x = "Hello";
+//intiialise
+let age;
+age = 30;
+//array of numbers
+let ids = [1, 2, 3, 4, 5];
+ids.push(4);
+//undeclared array
+let arr = [1, true, "hello"];
+//Tuple - specify exact types within an array
+let person = [1, "True", true];
+// Tuple with arrays
+let employee;
+employee = [
+    [1, "hassan"],
+    [3, "bahati"],
+];
+//Unions - variable with more than one type
+let identification = 22;
+identification = "22";
+//Enums(enumerated type) - define a set of named constants- if first is given a value of 1, the rest will count from that
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 4] = "Up";
+    Direction1[Direction1["Down"] = 5] = "Down";
+    Direction1[Direction1["Left"] = 6] = "Left";
+    Direction1[Direction1["Right"] = 7] = "Right";
+})(Direction1 || (Direction1 = {}));
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+// console.log(Direction2.Up)
+//Objects
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,48 @@
+// Basic types
 let id: number = 5;
+let company: string = "Hassan Bahati";
+let isPublished: boolean = true;
+let x: any = "Hello";
 
-console.log("ID: ", id);
+//intiialise
+let age: number;
+age = 30;
+
+//array of numbers
+let ids: number[] = [1, 2, 3, 4, 5];
+ids.push(4);
+
+//undeclared array
+let arr: any[] = [1, true, "hello"];
+
+//Tuple - specify exact types within an array
+let person: [number, string, boolean] = [1, "True", true];
+
+// Tuple with arrays
+let employee: [number, string][];
+employee = [
+  [1, "hassan"],
+  [3, "bahati"],
+];
+
+//Unions - variable with more than one type
+let identification: string | number = 22;
+identification = "22";
+
+//Enums(enumerated type) - define a set of named constants- if first is given a value of 1, the rest will count from that
+enum Direction1 {
+  Up = 4,
+  Down,
+  Left,
+  Right,
+}
+
+enum Direction2 {
+  Up = "Up",
+  Down = "Down",
+  Left = "Left",
+  Right = "Right",
+}
+// console.log(Direction2.Up)
+
+//Objects
